6a8dcdee94b8dffb25973754136438cf6421bc2c
Stepan <polyakovs@list.ru>
2022-05-06 23:23:29 +0700

    Add Test and PassOb PUUUUUSH!!

---
+++ Homework/src/chapter07/Test.java
@@ -0,0 +1,14 @@
+package chapter07;
+
+public class Test {
+    int a,b;
+    Test (int o, int j){
+        a = o;
+        b = j;
+    }
+    boolean equalTo(Test o) {
+        if (o.a == a && o.b == b) return true;
+            else return false;
+
+    }
+}
