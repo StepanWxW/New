f23265c6aadd28b046b46592a5da3e5762fa3963
Stepan <polyakovs@list.ru>
2022-05-06 23:08:50 +0700

    Add Box Ovelloadjava PusH ALl!)

---
+++ Homework/src/chapter07/Box.java
@@ -0,0 +1,21 @@
+package chapter07;
+
+public class Box {
+    double wigft;
+    double hight;
+    double depth;
+    Box (double w, double h, double d) {
+        wigft = w;
+        hight = h;
+        depth = d;
+    }
+    Box() {
+        wigft =-1;
+        hight=-1;
+        depth=-1;
+    }
+    Box (double len) {
+        wigft = hight =depth = len;
+    }
+    double volume () {return wigft * hight * depth;}
+}
